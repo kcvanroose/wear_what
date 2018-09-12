9edb4aec91b084c6b487dbce88851b7441453cb9
Ali Aziz <aliaziz@Alis-MacBook-Pro.local>
2018-09-12 10:26:38 +0100

    items created

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,35 +18,72 @@ categories = Category.create([
   {name: 'Trousers'},
   {name: 'T-shirt'},
   {name: 'Shirt'},
-  {name: 'Shoes'},
-  {name: 'Trousers'}
+  {name: 'Shoes'}
   ])
 puts 'Categories: ', Category.all.length
 
 
 users = User.create ([
-  {username: 'Casey'},
-  {username: 'Mike'},
-  {username: 'Jamie'}
+  {username: 'Casey'}
 ])
 
 puts 'Users: ', User.all.length
 
-items = Item.create ([
-  {image: 'scbnejkcbe', color: 'red', brand: 'LuLuLemon', user_id: 1, category_id: 1},
-  {image: 'scbnedwddwd', color: 'blue', brand: 'Burberry', user_id: 1, category_id: 2},
-  {image: 'scbnedwddwd', color: 'blue', brand: 'Burberry', user_id: 1, category_id: 3},
-  {image: 'cevfrvedwddwd', color: 'purple', brand: 'Cors', user_id: 1, category_id: 4}
-])
+# items = Item.create ([
+#   {image: 'scbnejkcbe', color: 'red', brand: 'LuLuLemon', user_id: 1, category_id: 1},
+#   {image: 'scbnedwddwd', color: 'blue', brand: 'Burberry', user_id: 1, category_id: 2},
+#   {image: 'scbnedwddwd', color: 'blue', brand: 'Burberry', user_id: 1, category_id: 3},
+#   {image: 'cevfrvedwddwd', color: 'purple', brand: 'Cors', user_id: 1, category_id: 4}
+# ])
 
 jacket = Item.new(color: 'black', brand: 'Canada Goose', user_id: 1, category_id: 3)
 f = File.open('app/views/Jacket.jpeg')
 jacket.image = f
 jacket.save
 
+shirtOne = Item.new(color: 'Camel', brand: 'Burberry', user_id: 1, category_id: 3)
+so = File.open('app/views/shirt-1.jpeg')
+shirtOne.image = so
+shirtOne.save
+
+shirtTwo = Item.new(color: 'Black', brand: 'Armani', user_id: 1, category_id: 3)
+st = File.open('app/views/shirt-2.jpeg')
+shirtTwo.image = st
+shirtTwo.save
+
+tshirtOne = Item.new(color: 'Navy Blue', brand: 'Polo Ralph Lauren', user_id: 1, category_id: 2)
+tso = File.open('app/views/t-shirt_1.jpeg')
+tshirtOne.image = tso
+tshirtOne.save
+
+tshirtTwo = Item.new(color: 'White', brand: 'Loewe', user_id: 1, category_id: 2)
+tst = File.open('app/views/t-shirt-2.jpeg')
+tshirtTwo.image = tst
+tshirtTwo.save
+
+trousersOne = Item.new(color: 'Blue', brand: 'J Brand', user_id: 1, category_id: 1)
+to = File.open('app/views/trousers-1.jpeg')
+trousersOne.image = to
+trousersOne.save
+
+trousersTwo = Item.new(color: 'Black', brand: 'Paige', user_id: 1, category_id: 1)
+tt = File.open('app/views/trousers-2.jpeg')
+trousersTwo.image = tt
+trousersTwo.save
+
+footwearOne = Item.new(color: 'Grey', brand: 'Giuseppe Zanotti', user_id: 1, category_id: 4)
+fo = File.open('app/views/footwear-1(trainers).jpeg')
+footwearOne.image = fo
+footwearOne.save
+
+footwearTwo = Item.new(color: 'Black', brand: 'Aldo', user_id: 1, category_id: 4)
+ft = File.open('app/views/footwear-2(shoes).jpeg)')
+footwearTwo.image = ft
+footwearTwo.save
+
+
+
 puts 'Items: ', Item.all.length
-
-
 
 outfits = Outfit.create ([
   {occasion: 'Date', user_id: 1},
