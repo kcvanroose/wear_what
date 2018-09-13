3c186c8941e520de494c8595192050d26b763a08
Casey Van-Roose <caseyvanroose@Caseys-MacBook-Pro.local>
2018-09-13 09:06:40 +0100

    category included in json

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username
   has_many :items
-  
-  class ItemSerializer < ActiveModel::Serializer
-    attributes :id, :image, :color, :brand, :category, :outfits
-  end
+  has_many :outfits
+
+
 end
